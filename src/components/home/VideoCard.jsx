541bf130d6685a146b5dcf2e29b9cefaab5716e4
krypton225 <k.mohamed.11.98@gmail.com>
2022-11-22 22:47:48 +0200

    ADD AOS lib & ADD animation for VideoCard

--- src/components/home/VideoCard.jsx
+++ src/components/home/VideoCard.jsx
@@ -1,9 +1,19 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 import CardInfo from "./CardInfo";
 import Thumbnail from "./Thumbnail";
 
 const VideoCard = ({ videoId, channelTitle, publishTime, title, medium }) => {
+    useEffect(() => {
+        AOS.init({
+            duration: 400
+        });
+    }, []);
+
     return (
-        <a className="home__videos__card" href={`https://www.youtube.com/watch?v=${videoId}`}>
+        <a className="home__videos__card" href={`https://www.youtube.com/watch?v=${videoId}`} data-aos="zoom-in">
             <Thumbnail thumbImage={medium.url} />
             <CardInfo channelTitle={channelTitle} publishTime={publishTime} title={title} />
         </a>
