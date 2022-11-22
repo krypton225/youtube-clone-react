e903f1c24152a04738415ace187e67c0c26c5d3e
krypton225 <k.mohamed.11.98@gmail.com>
2022-11-22 22:56:15 +0200

    RESET color scrollbar & CHNG animation in VideoCard

--- src/components/home/VideoCard.jsx
+++ src/components/home/VideoCard.jsx
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 import CardInfo from "./CardInfo";
 import Thumbnail from "./Thumbnail";
@@ -13,7 +14,7 @@ const VideoCard = ({ videoId, channelTitle, publishTime, title, medium }) => {
     }, []);
 
     return (
-        <a className="home__videos__card" href={`https://www.youtube.com/watch?v=${videoId}`} data-aos="zoom-in">
+        <a className="home__videos__card" href={`https://www.youtube.com/watch?v=${videoId}`} data-aos="zoom-out">
             <Thumbnail thumbImage={medium.url} />
             <CardInfo channelTitle={channelTitle} publishTime={publishTime} title={title} />
         </a>
